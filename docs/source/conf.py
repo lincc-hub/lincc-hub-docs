87fd5c413ccaf711444139018ce33b85aed2892f
Aditi Chauhan <caditi97@gmail.com>
2021-12-02 20:30:15 -0800

    added sphinx githubpages extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_rtd_dark_mode"]
+extensions = ["sphinx_rtd_dark_mode","sphinx.ext.githubpages"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
